96b1c3c9a2c274289e20c51e7bcf78e794e07eb4
Trent Johnson <tdjohnson0722@gmail.com>
2016-06-30 17:32:40 -0500

    changed .get to function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-    $.get('http://www.omdbapi.com/?t=cool+runnings')
+  function getMovie(url){
+    $.get(url)
         .then(function(data) {
             console.log(data);
             console.log(data.Title);
@@ -16,38 +17,8 @@ $(document).ready(function() {
         }).catch(function(){
           console.log("failure");
         });
-        $.get('http://www.omdbapi.com/?t=good+will+hunting')
-            .then(function(data) {
-                console.log(data);
-                console.log(data.Title);
-                var title= data.Title;
-                var released= data.Released;
-                var poster=data.Poster;
-                var plot=data.Plot;
-                console.log(poster);
-                $(".container").append("<h2>"+title+"</h2>");
-                $(".container").append("<ul></ul");
-                $(".container ul:last").append("<li>Released: "+released+"</li>");
-                $(".container ul:last").append('<li><img src='+poster+'></img></li>');
-                $(".container ul:last").append("<li>"+plot+"</li>");
-            }).catch(function(){
-              console.log("failure");
-            });
-            $.get('http://www.omdbapi.com/?t=shawshank+redemption')
-                .then(function(data) {
-                    console.log(data);
-                    console.log(data.Title);
-                    var title= data.Title;
-                    var released= data.Released;
-                    var poster=data.Poster;
-                    var plot=data.Plot;
-                    console.log(poster);
-                    $(".container").append("<h2>"+title+"</h2>");
-                    $(".container").append("<ul></ul");
-                    $(".container ul:last").append("<li>Released: "+released+"</li>");
-                    $(".container ul:last").append('<li><img src='+poster+'></img></li>');
-                    $(".container ul:last").append("<li>"+plot+"</li>");
-                }).catch(function(){
-                  console.log("failure");
-                });
+    }
+getMovie('http://www.omdbapi.com/?t=cool+runnings');
+getMovie('http://www.omdbapi.com/?t=shawshank+redemption');
+getMovie('http://www.omdbapi.com/?t=good+will+hunting');
 });
